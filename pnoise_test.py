6ff7670b3ae7485a7cf637daf3a3e4920167f0d0
Andrew Fox <foxostro@gmail.com>
2012-02-23 16:34:56 -0800

    Added a Perlin Noise generator in pnoise.py and a test routine.

---
+++ pnoise_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# vim: et ts=4
+
+from PIL import Image
+import pnoise
+
+
+def generateImage(fn, w, h, freq, numOctaves):
+    """Generates an image file containing perlin noise.
+    fn   - File name for the image.
+    w    - Width of the image, in pixels.
+    h    - Height of the image, in pixels.
+    freq - The frequency of the noise.
+    numOctaves = The number of octaves of noise to use.
+    """
+    img = Image.new('RGB', (w,h))
+    pix = img.load()
+
+    for x in range(0, w):
+        for y in range(0, h):
+            n = pnoise.perlinNoiseWithMultipleOctaves(float(x)/w/freq,
+                                                      float(y)/h/freq,
+                                                      0.0,
+                                                      numOctaves)
+            c = int(n*128+127)
+            pix[x,y] = (c,c,c)
+
+    img.save(fn)
+
+generateImage("test.png", 128, 128, 0.4, 4)
